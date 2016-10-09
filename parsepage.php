f897951701647744600bdbb8c7e623f4ac19f97c
xho <xhoxho@gmail.com>
2016-10-09 17:09:37 +0200

    [sec] sanitize string

--- parsepage.php
+++ parsepage.php
@@ -11,7 +11,7 @@ if (empty($_GET['u'])) {
     die(json_encode(array('error' => 'Missing user')));
 }
 
-$u = $_GET['u'];
+$u = filter_var($_GET['u'], FILTER_SANITIZE_STRING);
 // $url = 'https://twitter.com/search?q=from%3A%40' . $u;
 $url = 'https://twitter.com/search?f=tweets&vertical=default&q=from%3A%40' . $u;
 $content = file_get_contents($url);
